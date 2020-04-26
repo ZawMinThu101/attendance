e7d70166b7770cb5899dadd237232bd9705d475f
ZawMinThu101 <62640028+ZawMinThu101@users.noreply.github.com>
2020-04-27 06:03:49 +0630

    added remotesql connection details

--- db/conn.php
+++ db/conn.php
@@ -1,9 +1,16 @@
 <?php
-    $host = 'localhost';
-    $db = 'attendance_db';
-    $user = 'root';
-    $pass = '';
-    $charset = 'utf8mb4';
+//Remote database connection
+$host = 'remotemysql.com';
+$db = 'zoIhO1Zwh0';
+$user = 'zoIhO1Zwh0';
+$pass = 'x96AsaM2em';
+$charset = 'utf8mb4';
+//Development part
+  //  $host = 'localhost';
+    //$db = 'attendance_db';
+    //$user = 'root';
+    //$pass = '';
+    //$charset = 'utf8mb4';
 
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
